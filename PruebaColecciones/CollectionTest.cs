dffe41986c66f2ab8c6336cde0b43f8e5ff98e81
estrellitaLop <estrella.lopez.guz@gmail.com>
2023-10-10 10:01:25 -0600

    Agregar archivos de proyecto.

---
+++ PruebaColecciones/CollectionTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaColecciones
+{
+    public  class CollectionTest
+    {
+     public static void Main()
+        {
+            // Cree una coleccion de frutas
+            Collection<string> Frutas = new Collection<string>();
+            Frutas.Add("Mango");
+            Frutas.Add("Manzana");
+            Frutas.Add("Pera");
+            Frutas.Add("Banano");
+
+            //Imprimir la collecion
+
+            Console.WriteLine("{0} Frutas :",Frutas.Count);
+            Display(Frutas);
+
+            //Agregar un elemento de la coleccion
+            Frutas.Insert(3, "Uva");
+            Display(Frutas);
+
+            //Verificar si en la coleccion esta naranja 
+            Console.WriteLine($"\nLa coleccion contiene la naranja :\n { Frutas.Contains("Naranja")}");
+
+            // remover en el indice 2 de la coleccion
+            Console.WriteLine("Se esta removiendo");
+
+            Frutas.RemoveAt(2);
+            Display(Frutas);
+        }
+
+        private static void Display(Collection<string> frutas)
+        {
+            Console.WriteLine();
+            foreach (string item in frutas)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---------");
+
+        }
+    }
+}
---
+++ PruebaColecciones/Program.cs
@@ -0,0 +1,8 @@
+﻿// Cree una coleccion de frutas
+
+//Imprimir la collecion
+//Agregar un elemento de la colccion 
+//Verificar si en la coleccion esta naranja 
+// remover en el indice 2 de la coleccion
+//Imprimir en la coleccion
+
